c4fc23338370350dbfa597f65fdfb739a4c73e5c
Duval Kilpatrick <mkilpatrick@yext.com>
2023-09-21 12:27:35 -0400

    chore: use tsup for build

--- packages/sites-components/vite.config.ts
+++ packages/sites-components/vite.config.ts
@@ -1,31 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
 
 export default defineConfig(() => (
  {
-    plugins: [
-      react(),
-      dts({
-        insertTypesEntry: true,
-      }),
-    ],
-    build: {
-      lib: {
-        entry: 'src/index.ts', 
-        name: 'sites-components',
-        formats: ['es', 'umd'],
-      },
-      rollupOptions: {
-        external: ['react', 'react-dom'],
-        output: {
-          globals: {
-            react: 'React',
-            'react-dom': 'ReactDOM',
-          },
-        },
-      },
-    },
+    plugins: [react()],
     test: {
       globals: true,
       environment: 'jsdom',
