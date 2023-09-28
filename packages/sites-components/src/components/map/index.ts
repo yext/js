67bbf3aae0415f379a5801d906fdf3e7b6ee576a
bhaines <bhaines@yext.com>
2023-09-28 17:03:50 -0400

    feat(sites-components): export map providers

--- packages/sites-components/src/components/map/index.ts
+++ packages/sites-components/src/components/map/index.ts
@@ -1,4 +1,5 @@
 export { Map, useMapContext } from "./map.js";
 export { Marker } from "./marker.js";
 export { Clusterer, useClusterContext } from "./clusterer.js";
+export { GoogleMaps, MapboxMaps, BingMaps, BaiduMaps } from "@yext/components-tsx-maps";
 export * from "./types.js";
