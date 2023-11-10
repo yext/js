dbb2d1be3fb2b586d661a35ae1e5eba301aba082
nmanu1 <88398086+nmanu1@users.noreply.github.com>
2023-11-10 09:57:43 -0500

    feat(sites-components): port over LexicalRichText component from react-components (#18)
    This PR copies over the `LexicalRichText` components from `react-components` without any functionality changes. The actual logic for the component wasn't changed, but some refactoring was done to match the structure of this repo:
    - split out types and methods into separate files to follow how other components are laid out
    - co-located the Storybook stories and fixtures with the component file
    - updated the types from Storybook since this repo uses v7 instead of v6
    - added file extensions to imports
    - added a default React import to each TSX file
    
    This repo does not use Tailwind, so the component doesn't have any styling when viewed in the Storybook site. A future PR will change all the styling from Tailwind to CSS/SCSS.
    
    J=SLAP-2992
    TEST=manual
    
    - Tested that a story for the component is added to the Storybook site, even though it isn't styled
    - `npm pack` the `sites-components` package and use it in a test-site with Tailwind. Saw that the component is rendered as expected with styling matching that in the `react-components` Storybook site

---
+++ packages/sites-components/src/components/richText/LexicalRichText.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Meta, StoryFn } from "@storybook/react";
+import { LexicalRichText } from ".";
+import { LEXICAL_RICH_TEXT_SERIALIZED_AST } from "./sampleData.js";
+
+const meta: Meta<typeof LexicalRichText> = {
+  title: "components/LexicalRichText",
+  component: LexicalRichText,
+};
+export default meta;
+
+export const Primary: StoryFn<typeof LexicalRichText> = (args) => (
+  <LexicalRichText {...args} />
+);
+Primary.args = {
+  serializedAST: JSON.stringify(LEXICAL_RICH_TEXT_SERIALIZED_AST),
+};
