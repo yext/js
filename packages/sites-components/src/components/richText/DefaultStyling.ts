baaf174a059fd5ce8477a6339d4afd40061fc615
Nidhi Manu <nmanu@yext.com>
2023-11-15 11:31:32 -0500

    Match alpha

--- packages/sites-components/src/components/richText/DefaultStyling.ts
+++ packages/sites-components/src/components/richText/DefaultStyling.ts
@@ -2,98 +2,100 @@ import { EditorThemeClasses } from "lexical";
 import styles from "./default-styling.module.css";
 
 /**
- * CSS class suffixes for the different Tokens that can occur within a Code Block.
- * Token Types come from the PrismJS library, which Lexical uses to power Code Blocks.
- * The Types include Comment, Variable, Operator, etc.
- */
-const styleSuffixesToTokenTypes = {
-  tokenAttr: ["atrule", "attr", "keyword"],
-  tokenProperty: [
-    "boolean",
-    "constant",
-    "deleted",
-    "number",
-    "property",
-    "symbol",
-    "tag",
-  ],
-  tokenComment: ["cdata", "comment", "doctype", "prolog"],
-  tokenFunction: ["class", "class-name", "function"],
-  tokenSelector: ["builtin", "char", "inserted", "selector", "string"],
-  tokenVariable: ["important", "namespace", "regex", "variable"],
-  tokenOperator: ["entity", "operator", "url"],
-  tokenPunctuation: ["punctuation"],
-};
-
-/**
  * Default styling to apply to the different types of Lexical Rich Text
  * Elements.
  */
 const DefaultNodeStyling: EditorThemeClasses = {
-  ...Object.fromEntries(
-    [
-      "characterLimit",
-      "code",
-      "hashtag",
-      "image",
-      "link",
-      "ltr",
-      "mark",
-      "markOverlap",
-      "paragraph",
-      "quote",
-      "rtl",
-      "table",
-      "tableCell",
-      "tableCellHeader",
-    ].map((node) => [node, getStyleFromSuffix(node)])
-  ),
-  codeHighlight: Object.fromEntries(
-    Object.entries(styleSuffixesToTokenTypes).flatMap(
-      ([styleSuffix, tokenTypes]) =>
-        tokenTypes.map((type) => [type, getStyleFromSuffix(styleSuffix)])
-    )
-  ),
+  characterLimit: styles["yext-default-richtextv2-theme__characterLimit"],
+  code: styles["yext-default-richtextv2-theme__code"],
+  codeHighlight: {
+    atrule: styles["yext-default-richtextv2-theme__tokenAttr"],
+    attr: styles["yext-default-richtextv2-theme__tokenAttr"],
+    boolean: styles["yext-default-richtextv2-theme__tokenProperty"],
+    builtin: styles["yext-default-richtextv2-theme__tokenSelector"],
+    cdata: styles["yext-default-richtextv2-theme__tokenComment"],
+    char: styles["yext-default-richtextv2-theme__tokenSelector"],
+    class: styles["yext-default-richtextv2-theme__tokenFunction"],
+    "class-name": styles["yext-default-richtextv2-theme__tokenFunction"],
+    comment: styles["yext-default-richtextv2-theme__tokenComment"],
+    constant: styles["yext-default-richtextv2-theme__tokenProperty"],
+    deleted: styles["yext-default-richtextv2-theme__tokenProperty"],
+    doctype: styles["yext-default-richtextv2-theme__tokenComment"],
+    entity: styles["yext-default-richtextv2-theme__tokenOperator"],
+    function: styles["yext-default-richtextv2-theme__tokenFunction"],
+    important: styles["yext-default-richtextv2-theme__tokenVariable"],
+    inserted: styles["yext-default-richtextv2-theme__tokenSelector"],
+    keyword: styles["yext-default-richtextv2-theme__tokenAttr"],
+    namespace: styles["yext-default-richtextv2-theme__tokenVariable"],
+    number: styles["yext-default-richtextv2-theme__tokenProperty"],
+    operator: styles["yext-default-richtextv2-theme__tokenOperator"],
+    prolog: styles["yext-default-richtextv2-theme__tokenComment"],
+    property: styles["yext-default-richtextv2-theme__tokenProperty"],
+    punctuation: styles["yext-default-richtextv2-theme__tokenPunctuation"],
+    regex: styles["yext-default-richtextv2-theme__tokenVariable"],
+    selector: styles["yext-default-richtextv2-theme__tokenSelector"],
+    string: styles["yext-default-richtextv2-theme__tokenSelector"],
+    symbol: styles["yext-default-richtextv2-theme__tokenProperty"],
+    tag: styles["yext-default-richtextv2-theme__tokenProperty"],
+    url: styles["yext-default-richtextv2-theme__tokenOperator"],
+    variable: styles["yext-default-richtextv2-theme__tokenVariable"],
+  },
   embedBlock: {
-    base: getStyleFromSuffix("embedBlock"),
-    focus: getStyleFromSuffix("embedBlockFocus"),
+    base: styles["yext-default-richtextv2-theme__embedBlock"],
+    focus: styles["yext-default-richtextv2-theme__embedBlockFocus"],
   },
-  heading: Object.fromEntries(
-    ["h1", "h2", "h3", "h4", "h5", "h6"].map((node) => [
-      node,
-      getStyleFromSuffix(node),
-    ])
-  ),
+  hashtag: styles["yext-default-richtextv2-theme__hashtag"],
+  heading: {
+    h1: styles["yext-default-richtextv2-theme__h1"],
+    h2: styles["yext-default-richtextv2-theme__h2"],
+    h3: styles["yext-default-richtextv2-theme__h3"],
+    h4: styles["yext-default-richtextv2-theme__h4"],
+    h5: styles["yext-default-richtextv2-theme__h5"],
+    h6: styles["yext-default-richtextv2-theme__h6"],
+  },
+  image: styles["yext-default-richtextv2-theme__image"],
+  link: styles["yext-default-richtextv2-theme__link"],
   list: {
-    listitem: getStyleFromSuffix("listItem"),
-    listitemChecked: getStyleFromSuffix("listItemChecked"),
-    listitemUnchecked: getStyleFromSuffix("listItemUnchecked"),
+    listitem: styles["yext-default-richtextv2-theme__listItem"],
+    listitemChecked: styles["yext-default-richtextv2-theme__listItemChecked"],
+    listitemUnchecked:
+      styles["yext-default-richtextv2-theme__listItemUnchecked"],
     nested: {
-      listitem: getStyleFromSuffix("nestedListItem"),
+      listitem: styles["yext-default-richtextv2-theme__nestedListItem"],
     },
-    olDepth: ["ol1", "ol2", "ol3", "ol4", "ol5"].map(getStyleFromSuffix),
-    ulDepth: ["ul1", "ul2", "ul3"].map(getStyleFromSuffix),
+    olDepth: [
+      styles["yext-default-richtextv2-theme__ol1"],
+      styles["yext-default-richtextv2-theme__ol2"],
+      styles["yext-default-richtextv2-theme__ol3"],
+      styles["yext-default-richtextv2-theme__ol4"],
+      styles["yext-default-richtextv2-theme__ol5"],
+    ],
+    ulDepth: [
+      styles["yext-default-richtextv2-theme__ul1"],
+      styles["yext-default-richtextv2-theme__ul2"],
+      styles["yext-default-richtextv2-theme__ul3"],
+    ],
   },
-  text: Object.fromEntries(
-    [
-      "bold",
-      "code",
-      "italic",
-      "strikethrough",
-      "subscript",
-      "superscript",
-      "underline",
-      "underlineStrikethrough",
-    ].map((node) => [node, getStyleFromSuffix(`text${capitalize(node)}`)])
-  ),
+  ltr: styles["yext-default-richtextv2-theme__ltr"],
+  mark: styles["yext-default-richtextv2-theme__mark"],
+  markOverlap: styles["yext-default-richtextv2-theme__markOverlap"],
+  paragraph: styles["yext-default-richtextv2-theme__paragraph"],
+  quote: styles["yext-default-richtextv2-theme__quote"],
+  rtl: styles["yext-default-richtextv2-theme__rtl"],
+  table: styles["yext-default-richtextv2-theme__table"],
+  tableCell: styles["yext-default-richtextv2-theme__tableCell"],
+  tableCellHeader: styles["yext-default-richtextv2-theme__tableCellHeader"],
+  text: {
+    bold: styles["yext-default-richtextv2-theme__textBold"],
+    code: styles["yext-default-richtextv2-theme__textCode"],
+    italic: styles["yext-default-richtextv2-theme__textItalic"],
+    strikethrough: styles["yext-default-richtextv2-theme__textStrikethrough"],
+    subscript: styles["yext-default-richtextv2-theme__textSubscript"],
+    superscript: styles["yext-default-richtextv2-theme__textSuperscript"],
+    underline: styles["yext-default-richtextv2-theme__textUnderline"],
+    underlineStrikethrough:
+      styles["yext-default-richtextv2-theme__textUnderlineStrikethrough"],
+  },
 };
-
-function capitalize(str: string) {
-  return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
-}
-
-function getStyleFromSuffix(suffix: string) {
-  return styles[`yext-default-richtextv2-theme__${suffix}`];
-}
 
 export default DefaultNodeStyling;
