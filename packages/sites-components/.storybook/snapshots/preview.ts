87a811f9c999db3f036604669faf22d3cd078a60
Alex <alxtaing@gmail.com>
2023-11-22 14:33:35 -0500

    reorganize some things

--- packages/sites-components/.storybook/snapshots/preview.ts
+++
@@ -1,2 +0,0 @@
-import preview from "../preview";
-export default preview;
--- packages/sites-components/.storybook/wcag/test-runner.ts
+++ packages/sites-components/.storybook/wcag/test-runner.ts
@@ -1,6 +1,6 @@
 import { injectAxe, checkA11y } from "axe-playwright";
 import { Page } from "playwright-core";
-import { runOnly } from "./wcagConfig";
+import { runOnly } from "../wcagConfig";
 import { TestRunnerConfig } from "@storybook/test-runner";
 
 /**
