9b646d9e04ddbec50773d75813db1762d8ede850
Alex <alxtaing@gmail.com>
2023-11-22 14:28:00 -0500

    simplify

--- packages/sites-components/.storybook/wcag/main.ts
+++ packages/sites-components/.storybook/wcag/main.ts
@@ -1,10 +1,9 @@
 import baseConfig from "../main";
 import type { StorybookConfig } from "@storybook/react-vite";
 
-const stories = baseConfig.stories.map((glob) => `../${glob}`);
 const config: StorybookConfig = {
   ...baseConfig,
-  stories,
+  stories: baseConfig.stories.map((glob) => `../${glob}`),
 };
 
 export default config;
