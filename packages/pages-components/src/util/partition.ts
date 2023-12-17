83f846aaffc358bcef04989c5fdb2b48654a7e5f
Duval Kilpatrick <mkilpatrick@yext.com>
2023-12-17 13:00:26 -0500

    add region partition

---
+++ packages/pages-components/src/util/partition.ts
@@ -0,0 +1,16 @@
+/**
+ * Returns the partition of the provided accountId, either US or EU.
+ * If the partition cannot be determined, return an empty string.
+ *
+ * @param accountId The accountId for which to determine the partition.
+ */
+export const getPartition = (accountId: number) => {
+    switch (Math.floor((accountId / 100000000) % 10)) {
+      case 0:
+        return "US";
+      case 1:
+        return "EU";
+      default:
+        return "";
+    }
+  };
