ff778282b0b2ba5ee58f01ffea606acf96e26094
Duval Kilpatrick <mkilpatrick@yext.com>
2023-12-18 10:51:32 -0700

    minor lint fix

--- packages/pages-components/src/util/partition.ts
+++ packages/pages-components/src/util/partition.ts
@@ -2,7 +2,7 @@
  * Returns the partition of the provided accountId, either US or EU.
  * If the partition cannot be determined, return an empty string.
  *
- * @param accountId The accountId for which to determine the partition.
+ * @param accountId - The accountId for which to determine the partition.
  */
 export const getPartition = (accountId: number) => {
   switch (Math.floor((accountId / 100000000) % 10)) {
