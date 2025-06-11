417fb052df0ccda721ea41b90a47261e7eb818c7
Ben Life <blife@yext.com>
2025-06-11 17:19:05 -0400

    remove unused var

--- packages/pages-components/src/components/analytics/debugger.tsx
+++ packages/pages-components/src/components/analytics/debugger.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import { getRuntime } from "../../util/index.js";
 import type {
