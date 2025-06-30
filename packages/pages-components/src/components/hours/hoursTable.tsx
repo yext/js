81764509dfb67fe4d9a9bcd93b2adad1fb08a0fd
Ben Life <blife@yext.com>
2025-06-30 13:17:04 -0400

    lint

--- packages/pages-components/src/components/hours/hoursTable.tsx
+++ packages/pages-components/src/components/hours/hoursTable.tsx
@@ -196,10 +196,7 @@ const HoursTable: React.FC<HoursTableProps> = (props) => {
     hoursDays = collapseDays(hoursDays, props.dayOfWeekNames);
   }
 
-  const emptyStyle = React.useMemo(
-    () => ({ minHeight: `${hoursDays.length * 1.5}em` }),
-    [hoursDays.length]
-  );
+  const emptyStyle = { minHeight: `${hoursDays.length * 1.5}em` };
 
   return (
     <>
